6ce7b3e0843ffe686ca4bc13a2351f9e58e00f20
ayambabu23 <ab550@st-andrews.ac.uk>
2024-04-11 14:39:32 +0100

    Remove unnecessary audio variable

--- DeafBlindTactileQuiz/quiz.py
+++ DeafBlindTactileQuiz/quiz.py
@@ -8,9 +8,7 @@ def quiz():
     options_storage["left or right"] = random.randrange(2) # 0 = left, 1 = right
     options_storage["correct letter"] = random_letters[options_storage["left or right"]]
     options_storage["correct audio"] = options_storage["correct letter"] + ".mp3"
-    
     options_storage["image paths"] = [random_letter + ".jpg" for random_letter in options_storage["random letters"]]
-    options_storage["desc and audios"] = [(letter + "_description.txt", letter + "_description_audio.mp3") for letter in options_storage["random letters"]]
     
     options_storage["captions"] =   {'A': 'touch the end of their thumb with your right forefinger.',
                                      'B': 'place all your right hand finger tips in the middle of their left palm.',
