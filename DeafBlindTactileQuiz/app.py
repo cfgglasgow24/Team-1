e29129ec3fcf87431147ab76c32f6e3d83487b15
ayambabu23 <ab550@st-andrews.ac.uk>
2024-04-11 13:30:36 +0100

    Restored app.py

--- DeafBlindTactileQuiz/app.py
+++ DeafBlindTactileQuiz/app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('home.html')
+
+
+@app.route("/quiz/")
+def quiz():
+
+    return render_template('quiz.html')
 
 
 if __name__ == '__main__':
